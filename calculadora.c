f2001e6d5b6e23267c7abea0618adbb77b5404b0
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-01-25 22:59:11 -0300

    Add files via upload

--- calculadora.c
+++ calculadora.c
@@ -4,9 +4,12 @@
 float suma (float x, float y);
 float resta (float x, float y);
 float producto (float x, float y);
-float division (float x, float y);
+void division (float x, float y);
 float raiz (float x);
-float exponente (float x, float y);
+void exponente (float x, float y);
+float coseno (float x);
+float seno (float x);
+float tangente (float x);
 
 int main(){
 	
@@ -17,38 +20,38 @@ int main(){
 	
 	while (exp != 10){
 		
-		printf ("\n\n--MENU-- \n 1. Suma \n 2. Resta \n 3. Multiplicacion \n 4. Division \n 5. Raiz cuadrada \n 6. Exponente \n 10. Salir \n Escoja una opcion: ");
+		printf ("\n\n--MENU-- \n 1. Suma \n 2. Resta \n 3. Multiplicacion \n 4. Division \n 5. Raiz cuadrada \n 6. Exponente \n 7. Coseno \n 8. Seno \n 9. Tangente \n 10. Salir \n Escoja una opcion: ");
 		scanf("%d",&exp);
 	
 		switch (exp){
 			
 			case 1 : printf("Primer numero: ");
-				 	scanf("%f",&a);
-				 	printf("Segundo numero: ");
-				 	scanf("%f",&b);
-				 	printf ("%.2f",suma(a,b));
-				 	break;
+				 	 scanf("%f",&a);
+				 	 printf("Segundo numero: ");
+				 	 scanf("%f",&b);
+				 	 printf ("%.2f",suma(a,b));
+				 	 break;
 				 
 			case 2 : printf("Primer numero: ");
-				 	scanf("%f",&a);
-				 	printf("Segundo numero: ");
-				 	scanf("%f",&b);
-				 	printf ("%.2f",resta(a,b));
-				 	break;
+				 	 scanf("%f",&a);
+				 	 printf("Segundo numero: ");
+				 	 scanf("%f",&b);
+				 	 printf ("%.2f",resta(a,b));
+				 	 break;
 		
 			case 3 : printf("Primer numero: ");
-				 	scanf("%f",&a);
-				 	printf("Segundo numero: ");
-				 	scanf("%f",&b);
-				 	printf ("%.2f",producto(a,b));
-				 	break;
+				 	 scanf("%f",&a);
+				 	 printf("Segundo numero: ");
+				 	 scanf("%f",&b);
+				 	 printf ("%.2f",producto(a,b));
+				 	 break;
 				 	
 			case 4 : printf("Ingrese dividendo: ");
-				 	scanf("%f",&a);
-				 	printf("Ingrese divisor: ");
-				 	scanf("%f",&b);
-				 	printf ("%.2f",division(a,b));
-				 	break;
+				 	 scanf("%f",&a);
+				 	 printf("Ingrese divisor: ");
+				 	 scanf("%f",&b);
+				 	 division(a,b);
+				 	 break;
 				 	
 			case 5 : printf("Ingrese la raiz: ");
 					 scanf("%f",&a);	
@@ -59,11 +62,25 @@ int main(){
 					 scanf ("%f",&a);
 					 printf("Ingrese el exponente: ");
 					 scanf ("%f",&b);
-					 printf ("%.2f",exponente(a,b));
+					 exponente(a,b);
 					 break;
-					 		 
+			
+			case 7 : printf("Ingrese el numero: ");
+					 scanf("%f",&a);
+					 printf("%.2f",coseno(a));
+					 break;		 		 
+			
+			case 8 : printf("Ingrese el numero: ");
+					 scanf("%f",&a);
+					 printf("%.2f",seno(a));
+					 break;
+					
+			case 9 : printf ("Ingrese el numero: ");
+					 scanf("%f",&a);
+					 printf("%.2f",tangente(a));
+					 break;
 					 
-			case 10: printf ("Adios.");
+			case 10: printf ("\nAdios.");
 					 break;	 
 		}
 	}
@@ -84,11 +101,11 @@ float producto (float x, float y){
 	return x * y;
 }
 
-float division (float x, float y){
+void division (float x, float y){
 	if (y == 0){
-		return -1; // indeterminado 0/0
+		printf ("Error matematico"); // indeterminado 0/0
 	}else{
-		return x / y;
+		printf("%.2f",x / y);
 	}	
 }
 
@@ -96,19 +113,31 @@ float raiz (float x){
 	return sqrt(x);
 }
 
-float exponente (float x, float y){
+void exponente (float x, float y){
 	if (x == 0 && y == 0){
-		return -1; // indeterminado 0^0
+		printf ("Error matematico"); // indeterminado 0^0
 	}else{
 		if (y == 0){
-			return 1; // todo numero elevado a la 0 es 1, osea n^0 = 1
+			printf("1"); // todo numero elevado a la 0 es 1, osea n^0 = 1
 		}else{
 			int i, aux = x;
 		
 			for (i = 1; i< y; i++){
 				x = x * aux;
 			}
-			return x;
+			printf("%.2f",x);
 		}
 	}
+}
+
+float coseno (float x){
+	return cos (x);
+}
+
+float seno (float x){
+	return sin(x);
+}
+
+float tangente (float x){
+	return tan(x);
 }
