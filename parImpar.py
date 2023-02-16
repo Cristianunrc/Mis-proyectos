28795a59256169da694cdfe0ec6c8c6dfe7ba49c
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-02-16 14:12:44 -0300

    Add files via upload

---
+++ parImpar.py
@@ -0,0 +1,9 @@
+print("##--## Number pair or not pair ##--##")
+
+num = input("Enter the number: ")
+num = (int(num)) # casting
+
+if num % 2 == 0:
+ print("Is a pair number.")
+else:
+ print("Is a odd number.") # number impar
