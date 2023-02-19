82c82566c79053470bb583eb89e437afb27952a2
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-02-19 19:51:39 -0300

    Imprime los valores de un arreglo

--- arrays.py
+++ arrays.py
@@ -1,5 +1,5 @@
 array = []
 array = [1, 2, True, "Cristian", 0.5, 'C'] # un arreglo de tipos basicos, int, bool, string, float y char
-i = 0
+
 for i in range(len(array)):
     print(array[i]) # imprime el arreglo
